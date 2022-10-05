98792681b677bca84415fdb19f7dcea054222a0a
Dr.Caduceus <GitHub@anuneet.ml>
2022-10-05 20:44:48 +0530

    Minor

--- ddl.py
+++ ddl.py
@@ -14,7 +14,12 @@ import os
 from config import log, Vars
 
 # Setup Logger
-log.basicConfig(level=log.INFO, filename='runtime-log.txt',format='%(asctime)s:%(levelname)s:%(message)s', encoding='utf-8')
+log.basicConfig(
+    level=log.INFO,
+    datefmt="%d/%m/%Y %H:%M:%S",
+    format="[%(asctime)s][%(levelname)s] => %(message)s",
+    handlers=[log.StreamHandler(stream=sys.stdout),
+              log.FileHandler("runtime-log.txt", mode="a", encoding="utf-8")],)
 
 UPTOBOX_TOKEN = Vars[8]
 
